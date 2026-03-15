fc8a1257c3caf4f876462e2d6e2377de4b3b401a
DanOrsborne <54726325+DanOrsborne@users.noreply.github.com>
2026-03-15 16:20:09 +0000

    Update

--- mopidy_pidiv2/frontend.py
+++ mopidy_pidiv2/frontend.py
@@ -15,7 +15,7 @@ from .brainz import Brainz
 logger = logging.getLogger(__name__)
 
 
-class PiDiV2Config:
+class PiDiConfig:
     def __init__(self, config=None):
         self.rotation = config.get("rotation", 90)
         self.spi_port = 0
@@ -35,7 +35,7 @@ class PiDiV2Frontend(pykka.ThreadingActor, core.CoreListener):
         self.current_track = None
 
     def on_start(self):
-        self.display = PiDiV2(self.config)
+        self.display = PiDi(self.config)
         self.display.start()
         self.display.update(volume=self.core.mixer.get_volume().get())
         if "http" in self.config:
@@ -56,7 +56,7 @@ class PiDiV2Frontend(pykka.ThreadingActor, core.CoreListener):
                             break
                 if hostname is not None:
                     self.display.update(
-                        title=f"[PIDIV2] Visit http://{hostname}:{port}"
+                        title=f"Visit http://{hostname}:{port} to select content."
                     )
                     self.display.update_album_art(art="")
 
@@ -166,11 +166,11 @@ class PiDiV2Frontend(pykka.ThreadingActor, core.CoreListener):
         self.display.update(volume=volume)
 
 
-class PiDiV2:
+class PiDi:
     def __init__(self, config):
         self.config = config
         self.cache_dir = Extension.get_data_dir(config)
-        self.display_config = PiDiV2Config(config["pidiv2"])
+        self.display_config = PiDiConfig(config["pidiv2"])
         self.display_class = Extension.get_display_types()[
             self.config["pidiv2"]["display"]
         ]
